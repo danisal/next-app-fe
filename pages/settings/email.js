1a1852200e2b333663de9d8414af74bc7efa16c2
Daniel <daniel.salvado@gmail.com>
2019-10-02 23:15:22 +0200

    Add email settings page

--- pages/settings/email.js
+++ pages/settings/email.js
@@ -1,7 +1,33 @@
+import styled from '@emotion/styled';
+
 import { LoggedInContainer } from '../../components';
+import { Button, TextField } from '../../styled-components';
+
+const Wrapper = styled.section`
+    max-width: 600px;
+    margin: 24px auto;
+    width: 100%;
+
+    @media only screen and (max-width: ${props => props.theme.breakpoints.md}) {
+        padding: 0 1rem;
+    }
+`;
 
 function Email() {
-    return <LoggedInContainer>Email settings</LoggedInContainer>;
+    return (
+        <LoggedInContainer>
+            <Wrapper>
+                <TextField
+                    label="Current"
+                    type="email"
+                    value="email@domain.com"
+                    disabled
+                />
+                <TextField label="Email" type="email" />
+                <Button type="primary">Save</Button>
+            </Wrapper>
+        </LoggedInContainer>
+    );
 }
 
 export default Email;
