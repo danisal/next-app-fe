d96d468eb6088831faecadd05a5e0c06f293c9b9
Daniel <daniel.salvado@gmail.com>
2019-10-06 16:48:25 +0200

    Correct label case

--- pages/register.js
+++ pages/register.js
@@ -8,7 +8,7 @@ function Register() {
     return (
         <Form title="Register">
             <TextField required type="password" label="Password" />
-            <TextField required type="password" label="Repeat Password" />
+            <TextField required type="password" label="Repeat password" />
             {/* // TODO: try to pass the margin as css, not style */}
             <Button
                 style={{ marginTop: `3rem` }}
