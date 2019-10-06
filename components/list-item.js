a1a789fbdcea8952fb4c1fa3c450160af14d1604
Daniel <daniel.salvado@gmail.com>
2019-10-06 12:04:51 +0200

    Adjust list-item component

--- components/list-item.js
+++ components/list-item.js
@@ -2,23 +2,51 @@ import Link from 'next/link';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
-import Chevron from '../static/icons/chevron.svg';
+import ChevronRight from '../static/icons/chevron-right.svg';
 
 const StyledListItem = styled.li`
     cursor: pointer;
-    border-top: 1px solid ${props => props.theme.colors.darkGrey[600]};
+    transition: ease 0.05s;
 
     :active,
     :hover {
-        background-color: ${props => props.theme.colors.darkGrey[200]};
+        background-color: ${props => props.theme.colors.royalBlue[800]};
+
+        a {
+            color: ${props => props.theme.colors.royalBlue[100]};
+
+            span {
+                background-color: ${props => props.theme.colors.royalBlue[100]};
+
+                svg {
+                    color: ${props => props.theme.colors.royalBlue[800]};
+                }
+            }
+        }
     }
 
     a {
-        padding: 8px 16px;
+        padding: 16px;
         display: flex;
         align-items: center;
         justify-content: space-between;
         text-decoration: none;
+        color: ${props => props.theme.colors.royalBlue[800]};
+
+        span {
+            background-color: ${props => props.theme.colors.royalBlue[800]};
+            border-radius: 50%;
+            min-height: 32px;
+            min-width: 32px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            svg {
+                color: ${props => props.theme.colors.royalBlue[100]};
+                height: 16px;
+            }
+        }
     }
 `;
 
@@ -28,7 +56,9 @@ function ListItem({ children, to }) {
             <Link href={to}>
                 <a>
                     {children}
-                    <Chevron height={14} />
+                    <span>
+                        <ChevronRight />
+                    </span>
                 </a>
             </Link>
         </StyledListItem>
